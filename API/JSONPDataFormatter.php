1bd898b2690065a8055dcc4209ecb8d3df532ca2
Kadir Özdemir <kadir_oezdemir@hotmail.com>
2013-02-08 15:08:38 +0100

    JSONPDataFormatte
    So you can send also jsonp requests to the the RestfulServer

---
+++ API/JSONPDataFormatter.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * So you can send also jsonp requests to the the RestfulServer
+ * 
+ * @example:
+ * domain.com/api/v1/Licence.jsonp/?Licence=123456
+ */
+class JSONPDataFormatter extends JSONDataFormatter {
+	protected $outputContentType = 'application/json';
+
+	public function supportedExtensions() {
+		return array(
+			'jsonp'
+		);
+	}
+	public function convertDataObjectSet(SS_List $set, $fields = null) {
+		$fromparent = parent::convertDataObjectSet($set,$fields);
+		$json = (isset($_REQUEST["callback"])?$_REQUEST["callback"]:'callback'." (".$fromparent.");\n";
+		return $json;
+	}
+}
